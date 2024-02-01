0652d0519849c2721796bba6ff21b1d4e6b100c0
Ptaszek1 <mariusz.ptaszek2@gmail.com>
2024-02-01 15:58:46 +0100

    Changed fonts place etc

--- src/data/PortfolioData/PortfolioData.tsx
+++ src/data/PortfolioData/PortfolioData.tsx
@@ -1,132 +1,132 @@
 // Catering Gorąco Polecam
 
-import $cgp from "../../assets/images/portfolio/cgp/goraco-polecam.jpg";
-import $cgpSmall from "../../assets/images/portfolio/cgp/goraco-polecam-370x310.jpg";
-import $cgpHome from "../../assets/images/portfolio/cgp/gp-sg.png";
-import $cgpHomeSmall from "../../assets/images/portfolio/cgp/gp-sg-370x310.png";
-import $cgpCart from "../../assets/images/portfolio/cgp/gp-koszyk.png";
-import $cgpCartSmall from "../../assets/images/portfolio/cgp/gp-koszyk-370x310.png";
-import $cgpSingle from "../../assets/images/portfolio/cgp/gp-single-product.png";
-import $cgpSingleSmall from "../../assets/images/portfolio/cgp/gp-single-product-370x310.png";
+import $cgp from "/images/portfolio/cgp/goraco-polecam.jpg";
+import $cgpSmall from "/images/portfolio/cgp/goraco-polecam-370x310.jpg";
+import $cgpHome from "/images/portfolio/cgp/gp-sg.png";
+import $cgpHomeSmall from "/images/portfolio/cgp/gp-sg-370x310.png";
+import $cgpCart from "/images/portfolio/cgp/gp-koszyk.png";
+import $cgpCartSmall from "/images/portfolio/cgp/gp-koszyk-370x310.png";
+import $cgpSingle from "/images/portfolio/cgp/gp-single-product.png";
+import $cgpSingleSmall from "/images/portfolio/cgp/gp-single-product-370x310.png";
 
 // MR - Medyczna rejestracja
 
-import $mr from "../../assets/images/portfolio/mr/medycznarejestracja1.jpg";
-import $mrSmall from "../../assets/images/portfolio/mr/medycznarejestracja1-370x310.jpg";
-import $mrBlog from "../../assets/images/portfolio/mr/mr-blog.png";
-import $mrBlogSmall from "../../assets/images/portfolio/mr/mr-blog-370x310.png";
-import $mrHome from "../../assets/images/portfolio/mr/mr-strona-glowna.png";
-import $mrHomeSmall from "../../assets/images/portfolio/mr/mr-strona-glowna-370x310.png";
-import $mrBenefits from "../../assets/images/portfolio/mr/mr-korzysci.png";
-import $mrBenefitsSmall from "../../assets/images/portfolio/mr/mr-korzysci-370x310.png";
+import $mr from "/images/portfolio/mr/medycznarejestracja1.jpg";
+import $mrSmall from "/images/portfolio/mr/medycznarejestracja1-370x310.jpg";
+import $mrBlog from "/images/portfolio/mr/mr-blog.png";
+import $mrBlogSmall from "/images/portfolio/mr/mr-blog-370x310.png";
+import $mrHome from "/images/portfolio/mr/mr-strona-glowna.png";
+import $mrHomeSmall from "/images/portfolio/mr/mr-strona-glowna-370x310.png";
+import $mrBenefits from "/images/portfolio/mr/mr-korzysci.png";
+import $mrBenefitsSmall from "/images/portfolio/mr/mr-korzysci-370x310.png";
 
 // Belotto
 
-import $belotto from "../../assets/images/portfolio/belotto/belotto.png";
-import $belottoSmall from "../../assets/images/portfolio/belotto/belotto-370x310.png";
-import $belottoHome from "../../assets/images/portfolio/belotto/belotto-home.png";
-import $belottoHomeSmall from "../../assets/images/portfolio/belotto/belotto-home-370x310.png";
-import $belottoBlog from "../../assets/images/portfolio/belotto/belotto-blog.png";
-import $belottoBlogSmall from "../../assets/images/portfolio/belotto/belotto-blog-370x310.png";
-import $belottoDescription from "../../assets/images/portfolio/belotto/belotto-opis.png";
-import $belottoDescriptionSmall from "../../assets/images/portfolio/belotto/belotto-opis-370x310.png";
-import $belottoPortfolio from "../../assets/images/portfolio/belotto/belotto-portfolio.png";
-import $belottoPortfolioSmall from "../../assets/images/portfolio/belotto/belotto-portfolio-370x310.png";
-import $belottoTeam from "../../assets/images/portfolio/belotto/belotto-zespol.png";
-import $belottoTeamSmall from "../../assets/images/portfolio/belotto/belotto-zespol-370x310.png";
+import $belotto from "/images/portfolio/belotto/belotto.png";
+import $belottoSmall from "/images/portfolio/belotto/belotto-370x310.png";
+import $belottoHome from "/images/portfolio/belotto/belotto-home.png";
+import $belottoHomeSmall from "/images/portfolio/belotto/belotto-home-370x310.png";
+import $belottoBlog from "/images/portfolio/belotto/belotto-blog.png";
+import $belottoBlogSmall from "/images/portfolio/belotto/belotto-blog-370x310.png";
+import $belottoDescription from "/images/portfolio/belotto/belotto-opis.png";
+import $belottoDescriptionSmall from "/images/portfolio/belotto/belotto-opis-370x310.png";
+import $belottoPortfolio from "/images/portfolio/belotto/belotto-portfolio.png";
+import $belottoPortfolioSmall from "/images/portfolio/belotto/belotto-portfolio-370x310.png";
+import $belottoTeam from "/images/portfolio/belotto/belotto-zespol.png";
+import $belottoTeamSmall from "/images/portfolio/belotto/belotto-zespol-370x310.png";
 
 // Lionsgarage
 
-import $lg from "../../assets/images/portfolio/lg/lions-garage-home1.png";
-import $lgSmall from "../../assets/images/portfolio/lg/lions-garage-home1-370x310.png";
-import $lgHome from "../../assets/images/portfolio/lg/lionsgarage-home.png";
-import $lgHomeSmall from "../../assets/images/portfolio/lg/lionsgarage-home-370x310.png";
-import $lgContact from "../../assets/images/portfolio/lg/lionsgarage-kontakt.png";
-import $lgContactSmall from "../../assets/images/portfolio/lg/lionsgarage-kontakt-370x310.png";
-import $lgAbout from "../../assets/images/portfolio/lg/lionsgarage-o-nas.png";
-import $lgAboutSmall from "../../assets/images/portfolio/lg/lionsgarage-o-nas-370x310.png";
-import $lgWork from "../../assets/images/portfolio/lg/lionsgarage-realizacje.png";
-import $lgWorkSmall from "../../assets/images/portfolio/lg/lionsgarage-realizacje-370x310.png";
-import $lgServices from "../../assets/images/portfolio/lg/lionsgarage-uslugi.png";
-import $lgServicesSmall from "../../assets/images/portfolio/lg/lionsgarage-uslugi-370x310.png";
+import $lg from "/images/portfolio/lg/lions-garage-home1.png";
+import $lgSmall from "/images/portfolio/lg/lions-garage-home1-370x310.png";
+import $lgHome from "/images/portfolio/lg/lionsgarage-home.png";
+import $lgHomeSmall from "/images/portfolio/lg/lionsgarage-home-370x310.png";
+import $lgContact from "/images/portfolio/lg/lionsgarage-kontakt.png";
+import $lgContactSmall from "/images/portfolio/lg/lionsgarage-kontakt-370x310.png";
+import $lgAbout from "/images/portfolio/lg/lionsgarage-o-nas.png";
+import $lgAboutSmall from "/images/portfolio/lg/lionsgarage-o-nas-370x310.png";
+import $lgWork from "/images/portfolio/lg/lionsgarage-realizacje.png";
+import $lgWorkSmall from "/images/portfolio/lg/lionsgarage-realizacje-370x310.png";
+import $lgServices from "/images/portfolio/lg/lionsgarage-uslugi.png";
+import $lgServicesSmall from "/images/portfolio/lg/lionsgarage-uslugi-370x310.png";
 
 // Norma machinery
 
-import $nm from "../../assets/images/portfolio/nm/normamachinery-home.png";
-import $nmSmall from "../../assets/images/portfolio/nm/normamachinery-home-370x310.png";
-import $nmContact from "../../assets/images/portfolio/nm/normamachinery-kontakt.png";
-import $nmContactSmall from "../../assets/images/portfolio/nm/normamachinery-kontakt-370x310.png";
-import $nmOffer from "../../assets/images/portfolio/nm/normamachinery-oferta.png";
-import $nmOfferSmall from "../../assets/images/portfolio/nm/normamachinery-oferta-370x310.png";
-import $nmAbout from "../../assets/images/portfolio/nm/normamachinery-o-firmie.png";
-import $nmAboutSmall from "../../assets/images/portfolio/nm/normamachinery-o-firmie-370x310.png";
+import $nm from "/images/portfolio/nm/normamachinery-home.png";
+import $nmSmall from "/images/portfolio/nm/normamachinery-home-370x310.png";
+import $nmContact from "/images/portfolio/nm/normamachinery-kontakt.png";
+import $nmContactSmall from "/images/portfolio/nm/normamachinery-kontakt-370x310.png";
+import $nmOffer from "/images/portfolio/nm/normamachinery-oferta.png";
+import $nmOfferSmall from "/images/portfolio/nm/normamachinery-oferta-370x310.png";
+import $nmAbout from "/images/portfolio/nm/normamachinery-o-firmie.png";
+import $nmAboutSmall from "/images/portfolio/nm/normamachinery-o-firmie-370x310.png";
 
 // Tłumaczenia
 
-import $translate from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-home.png";
-import $translateSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-home-370x310.png";
-import $translateContact from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-kontakt.png";
-import $translateContactSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-kontakt-370x310.png";
-import $translateAbout from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-o-nas.png";
-import $translateAboutSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-o-nas-370x310.png";
-import $translateOffer from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-oferta.png";
-import $translateOfferSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-oferta-370x310.png";
-import $translateClients from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-klienci.png";
-import $translateClientsSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-klienci-370x310.png";
-import $translatePrices from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-cennik.png";
-import $translatePricesSmall from "../../assets/images/portfolio/tlumaczenia/tlumaczenia-cennik-370x310.png";
+import $translate from "/images/portfolio/tlumaczenia/tlumaczenia-home.png";
+import $translateSmall from "/images/portfolio/tlumaczenia/tlumaczenia-home-370x310.png";
+import $translateContact from "/images/portfolio/tlumaczenia/tlumaczenia-kontakt.png";
+import $translateContactSmall from "/images/portfolio/tlumaczenia/tlumaczenia-kontakt-370x310.png";
+import $translateAbout from "/images/portfolio/tlumaczenia/tlumaczenia-o-nas.png";
+import $translateAboutSmall from "/images/portfolio/tlumaczenia/tlumaczenia-o-nas-370x310.png";
+import $translateOffer from "/images/portfolio/tlumaczenia/tlumaczenia-oferta.png";
+import $translateOfferSmall from "/images/portfolio/tlumaczenia/tlumaczenia-oferta-370x310.png";
+import $translateClients from "/images/portfolio/tlumaczenia/tlumaczenia-klienci.png";
+import $translateClientsSmall from "/images/portfolio/tlumaczenia/tlumaczenia-klienci-370x310.png";
+import $translatePrices from "/images/portfolio/tlumaczenia/tlumaczenia-cennik.png";
+import $translatePricesSmall from "/images/portfolio/tlumaczenia/tlumaczenia-cennik-370x310.png";
 
 // Iwona Durka
 
-import $id from "../../assets/images/portfolio/id/iwonadurka-home.png";
-import $idSmall from "../../assets/images/portfolio/id/iwonadurka-home-370x310.png";
-import $idAbout from "../../assets/images/portfolio/id/iwonadurka-about.png";
-import $idAboutSmall from "../../assets/images/portfolio/id/iwonadurka-about-370x310.png";
-import $idContact from "../../assets/images/portfolio/id/iwonadurka-contact.png";
-import $idContactSmall from "../../assets/images/portfolio/id/iwonadurka-contact-370x310.png";
-import $idWork from "../../assets/images/portfolio/id/iwonadurka-my-work.png";
-import $idWorkSmall from "../../assets/images/portfolio/id/iwonadurka-my-work-370x310.png";
-import $idPortfolio from "../../assets/images/portfolio/id/iwonadurka-portfolio.png";
-import $idPortfolioSmall from "../../assets/images/portfolio/id/iwonadurka-portfolio-370x310.png";
-import $idRealizations from "../../assets/images/portfolio/id/iwonadurka-realizacje.png";
-import $idRealizationsSmall from "../../assets/images/portfolio/id/iwonadurka-realizacje-370x310.png";
+import $id from "/images/portfolio/id/iwonadurka-home.png";
+import $idSmall from "/images/portfolio/id/iwonadurka-home-370x310.png";
+import $idAbout from "/images/portfolio/id/iwonadurka-about.png";
+import $idAboutSmall from "/images/portfolio/id/iwonadurka-about-370x310.png";
+import $idContact from "/images/portfolio/id/iwonadurka-contact.png";
+import $idContactSmall from "/images/portfolio/id/iwonadurka-contact-370x310.png";
+import $idWork from "/images/portfolio/id/iwonadurka-my-work.png";
+import $idWorkSmall from "/images/portfolio/id/iwonadurka-my-work-370x310.png";
+import $idPortfolio from "/images/portfolio/id/iwonadurka-portfolio.png";
+import $idPortfolioSmall from "/images/portfolio/id/iwonadurka-portfolio-370x310.png";
+import $idRealizations from "/images/portfolio/id/iwonadurka-realizacje.png";
+import $idRealizationsSmall from "/images/portfolio/id/iwonadurka-realizacje-370x310.png";
 
 // Bygger
 
-import $bygger from "../../assets/images/portfolio/bygger/bygger.jpg";
-import $byggerSmall from "../../assets/images/portfolio/bygger/bygger-370x310.jpg";
-import $byggerSecond from "../../assets/images/portfolio/bygger/bygger2.jpg";
-import $byggerSecondSmall from "../../assets/images/portfolio/bygger/bygger2-370x310.jpg";
-import $byggerThird from "../../assets/images/portfolio/bygger/bygger3.jpg";
-import $byggerThirdSmall from "../../assets/images/portfolio/bygger/bygger3-370x310.jpg";
+import $bygger from "/images/portfolio/bygger/bygger.jpg";
+import $byggerSmall from "/images/portfolio/bygger/bygger-370x310.jpg";
+import $byggerSecond from "/images/portfolio/bygger/bygger2.jpg";
+import $byggerSecondSmall from "/images/portfolio/bygger/bygger2-370x310.jpg";
+import $byggerThird from "/images/portfolio/bygger/bygger3.jpg";
+import $byggerThirdSmall from "/images/portfolio/bygger/bygger3-370x310.jpg";
 
 // Pani Flip
 
-import $pf from "../../assets/images/portfolio/pf/paniflip-home.png";
-import $pfSmall from "../../assets/images/portfolio/pf/paniflip-home-370x310.png";
-import $pfAbout from "../../assets/images/portfolio/pf/paniflip-omnie.png";
-import $pfAboutSmall from "../../assets/images/portfolio/pf/paniflip-omnie-370x310.png";
-import $pfContact from "../../assets/images/portfolio/pf/paniflip-kontakt.png";
-import $pfContactSmall from "../../assets/images/portfolio/pf/paniflip-kontakt-370x310.png";
-import $pfWork from "../../assets/images/portfolio/pf/paniflip-realizacje.png";
-import $pfWorkSmall from "../../assets/images/portfolio/pf/paniflip-realizacje-370x310.png";
-import $pfCo from "../../assets/images/portfolio/pf/paniflip-wspolpraca.png";
-import $pfCoSmall from "../../assets/images/portfolio/pf/paniflip-wspolpraca-370x310.png";
+import $pf from "/images/portfolio/pf/paniflip-home.png";
+import $pfSmall from "/images/portfolio/pf/paniflip-home-370x310.png";
+import $pfAbout from "/images/portfolio/pf/paniflip-omnie.png";
+import $pfAboutSmall from "/images/portfolio/pf/paniflip-omnie-370x310.png";
+import $pfContact from "/images/portfolio/pf/paniflip-kontakt.png";
+import $pfContactSmall from "/images/portfolio/pf/paniflip-kontakt-370x310.png";
+import $pfWork from "/images/portfolio/pf/paniflip-realizacje.png";
+import $pfWorkSmall from "/images/portfolio/pf/paniflip-realizacje-370x310.png";
+import $pfCo from "/images/portfolio/pf/paniflip-wspolpraca.png";
+import $pfCoSmall from "/images/portfolio/pf/paniflip-wspolpraca-370x310.png";
 
 // Lean technology
 
-import $lt from "../../assets/images/portfolio/lt/lean-technology-strona-glowna.png";
-import $ltSmall from "../../assets/images/portfolio/lt/lean-technology-strona-glowna-370x310.png";
-import $ltNews from "../../assets/images/portfolio/lt/lean-technology-aktualnosci.png";
-import $ltNewsSmall from "../../assets/images/portfolio/lt/lean-technology-aktualnosci-370x310.png";
-import $ltBlog from "../../assets/images/portfolio/lt/lean-technology-blog.png";
-import $ltBlogSmall from "../../assets/images/portfolio/lt/lean-technology-blog-370x310.png";
-import $ltContact from "../../assets/images/portfolio/lt/lean-technology-kontakt.png";
-import $ltContactSmall from "../../assets/images/portfolio/lt/lean-technology-kontakt-370x310.png";
-import $ltProducts from "../../assets/images/portfolio/lt/lean-technology-produkty.png";
-import $ltProductsSmall from "../../assets/images/portfolio/lt/lean-technology-produkty-370x310.png";
-import $ltSupport from "../../assets/images/portfolio/lt/lean-technology-wsparcie.png";
-import $ltSupportSmall from "../../assets/images/portfolio/lt/lean-technology-wsparcie-370x310.png";
+import $lt from "/images/portfolio/lt/lean-technology-strona-glowna.png";
+import $ltSmall from "/images/portfolio/lt/lean-technology-strona-glowna-370x310.png";
+import $ltNews from "/images/portfolio/lt/lean-technology-aktualnosci.png";
+import $ltNewsSmall from "/images/portfolio/lt/lean-technology-aktualnosci-370x310.png";
+import $ltBlog from "/images/portfolio/lt/lean-technology-blog.png";
+import $ltBlogSmall from "/images/portfolio/lt/lean-technology-blog-370x310.png";
+import $ltContact from "/images/portfolio/lt/lean-technology-kontakt.png";
+import $ltContactSmall from "/images/portfolio/lt/lean-technology-kontakt-370x310.png";
+import $ltProducts from "/images/portfolio/lt/lean-technology-produkty.png";
+import $ltProductsSmall from "/images/portfolio/lt/lean-technology-produkty-370x310.png";
+import $ltSupport from "/images/portfolio/lt/lean-technology-wsparcie.png";
+import $ltSupportSmall from "/images/portfolio/lt/lean-technology-wsparcie-370x310.png";
 
 const PortfolioData: {
 	id: number;
